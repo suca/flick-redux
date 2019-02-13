75bfbfba2b4a08349f8b1abce9780b483aba64ce
Omar Sucapuca <omar.sucapuca@betbright.com>
2019-02-13 10:49:00 +0000

    Adding homepage to the package.json for the "docs"

---
+++ docs/precache-manifest.2fafa769970ae216817e8bd47650c47f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b523d1333a370844c350",
+    "url": "./static/js/main.b523d133.chunk.js"
+  },
+  {
+    "revision": "91f9ac84cbf71dbb2aca",
+    "url": "./static/js/1.91f9ac84.chunk.js"
+  },
+  {
+    "revision": "b523d1333a370844c350",
+    "url": "./static/css/main.d936130a.chunk.css"
+  },
+  {
+    "revision": "c2448cc38cb2d94b793f12ad7b71dd00",
+    "url": "./index.html"
+  }
+];
